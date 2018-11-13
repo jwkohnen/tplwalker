c20e06d4e1ded40e146e3fa9a6edae55c786cec2
Johannes Kohnen <jwkohnen-github@ko-sys.com>
2018-11-13 17:39:11 +0000

    Fix package doc

--- doc.go
+++ doc.go
@@ -13,9 +13,9 @@
 //    You should have received a copy of the GNU General Public License along
 //    with tplwalker.  If not, see <http://www.gnu.org/licenses/>.
 
-// tplwalker is a tool to copy and transform a directory of templates and other
-// files to a target directory.  It will re-create subdirectories, regular
-// files and symlinks from a source directory.  Files that end with a
+// Package tplwalker is a tool to copy and transform a directory of templates
+// and other files to a target directory.  It will re-create subdirectories,
+// regular files and symlinks from a source directory.  Files that end with a
 // configurable suffix, will be stripped of that suffix and be digested as a Go
 // text template and executed with some data object.
 //
